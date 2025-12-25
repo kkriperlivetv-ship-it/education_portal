048df1429d69fed5ace5b95584d6c3ecd1d1c323
hollowsouls <kkriperlivetv@gmail.com>
2025-12-25 15:29:34 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-change-this'
-    
-    # ИЗМЕНЕНИЕ: Используем SQLite вместо MySQL
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///education_portal.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
